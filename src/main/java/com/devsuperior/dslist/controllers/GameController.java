0f16cdbd7edbf87b8c85459480ec90ffd4bc3990
Caique Martins <caiquemrtins@hotmail.com>
2023-05-10 16:07:16 -0300

    Get game by id

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -1,11 +1,13 @@
 package com.devsuperior.dslist.controllers;
 
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +25,12 @@ public class GameController {
         List<GameMinDTO> result = gameService.findAll();
         return result;
     }
+
+    @GetMapping(value = "/{id}") //aqui está falando que vai ter um parametro nessa requisição
+    public GameDTO findById(@PathVariable Long id){  //o path variable é para garantir que o id case com o id da requisição
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
+
 
 }
